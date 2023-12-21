50288c500745910c6999eeca8ac617b9978316e1
RishabhTibrewal <96077545+RishabhTibrewal@users.noreply.github.com>
2023-12-22 01:12:23 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <div className={footerStyles.main}>
-        <h1>Simula</h1>
+        <h1>SecureStake</h1>
         <Link href='https://github.com/anshss/defining-Defi'><h4 className={footerStyles.heading}>View Code</h4></Link>
     </div>
   )
